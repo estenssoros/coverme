7c42bdbfd7fbadf97685b698ac5a499286b0cd9d
estenssoros <seb.estenssoro@gmail.com>
2019-09-19 07:31:30 -0600

    don't need these

--- configuration.go
+++ configuration.go
@@ -5,8 +5,8 @@ import (
 )
 
 type configuration struct {
-	AppRoot            string        `yaml:"app_root" json:"app_root"`
-	IgnoredFolders     []string      `yaml:"ignored_folders" json:"ignored_folders"`
-	IncludedExtensions []string      `yaml:"included_extensions" json:"included_extensions"`
-	BuildDelay         time.Duration `yaml:"build_delay" json:"build_delay"`
+	AppRoot            string
+	IgnoredFolders     []string
+	IncludedExtensions []string
+	BuildDelay         time.Duration
 }
